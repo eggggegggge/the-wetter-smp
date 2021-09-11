e9dd6da017e82751914e43c2b1a700fc72912842
eggggegggge <oreli.throwaway@gmail.com>
2021-09-11 01:15:24 -0600

    fishing is now affected by luck (good luck will yield uncommon fish more commonly while bad luck will decrease chances of catching uncommon fish, and having no luck or both good and bad luck will yield regular results)

--- src/main/java/net/mcreator/thewetsmp/procedures/ColdOceanFishProcedure.java
+++ src/main/java/net/mcreator/thewetsmp/procedures/ColdOceanFishProcedure.java
@@ -64,6 +64,12 @@ public class ColdOceanFishProcedure {
 		double time = 0;
 		double altitude = 0;
 		double fish = 0;
+		double cod = 0;
+		double salmon = 0;
+		double scallop = 0;
+		double seaurchin = 0;
+		double crab = 0;
+		double jellyfish = 0;
 		/* fishing factors */
 		rain = (boolean) world.getWorldInfo().isRaining();
 		if ((new Object() {
@@ -116,7 +122,7 @@ public class ColdOceanFishProcedure {
 					}
 					return 0;
 				}
-			}.check(entity)) + 1);
+			}.check(entity)) + 2);
 		} else {
 			unluck = (double) 0;
 		}
@@ -124,14 +130,34 @@ public class ColdOceanFishProcedure {
 		rodpower = (double) FishingRodCheckProcedure.executeProcedure(ImmutableMap.of("entity", entity));
 		baitpower = (double) BaitCheckProcedure.executeProcedure(ImmutableMap.of("entity", entity, "world", world));/* loot factors */
 		time = (double) (world.getWorldInfo().getDayTime());
-		altitude = (double) (Math.floor(y));/* how many fish */
-		fish = (double) 37;
+		altitude = (double) (Math.floor(y));/* define base values */
+		cod = (double) 10;
+		salmon = (double) 18;
+		scallop = (double) 23;
+		seaurchin = (double) 30;
+		crab = (double) 37;
+		jellyfish = (double) 45;/* apply modifiers */
+		if ((totalluck > 1)) {
+			cod = (double) (cod - 3);
+			salmon = (double) (salmon - 2);
+			scallop = (double) (scallop + 5);
+			seaurchin = (double) (seaurchin + 4);
+			crab = (double) (crab + 0);
+			jellyfish = (double) (jellyfish + 2);
+		} else if ((totalluck < 0)) {
+			cod = (double) (cod + 3);
+			salmon = (double) (salmon + 2);
+			scallop = (double) (scallop - 2);
+			seaurchin = (double) (seaurchin - 4);
+			crab = (double) (crab + 2);
+			jellyfish = (double) (jellyfish - 3);
+		} /* fish */
+		fish = (double) crab;
 		if (((time > 13000) && (time < 23000))) {
-			fish = (double) 45;
+			fish = (double) jellyfish;
 		}
-		fish = (double) (fish * (0.5 * totalluck));
-		sel = (double) ((new Random()).nextInt((int) fish + 1));/* fish */
-		if ((sel < 10)) {
+		sel = (double) ((new Random()).nextInt((int) fish + 1));
+		if (((sel >= 0) && (sel <= cod))) {
 			{
 				Entity _ent = entity;
 				if (!_ent.world.isRemote && _ent.world.getServer() != null) {
@@ -147,7 +173,7 @@ public class ColdOceanFishProcedure {
 				if (mcserv != null)
 					mcserv.getPlayerList().func_232641_a_(new StringTextComponent("cod"), ChatType.SYSTEM, Util.DUMMY_UUID);
 			}
-		} else if (((sel >= 11) && (sel <= 18))) {
+		} else if (((sel >= (cod + 1)) && (sel <= salmon))) {
 			{
 				Entity _ent = entity;
 				if (!_ent.world.isRemote && _ent.world.getServer() != null) {
@@ -163,7 +189,7 @@ public class ColdOceanFishProcedure {
 				if (mcserv != null)
 					mcserv.getPlayerList().func_232641_a_(new StringTextComponent("salmon"), ChatType.SYSTEM, Util.DUMMY_UUID);
 			}
-		} else if (((sel >= 19) && (sel <= 23))) {
+		} else if (((sel >= (salmon + 1)) && (sel <= scallop))) {
 			{
 				Entity _ent = entity;
 				if (!_ent.world.isRemote && _ent.world.getServer() != null) {
@@ -179,7 +205,7 @@ public class ColdOceanFishProcedure {
 				if (mcserv != null)
 					mcserv.getPlayerList().func_232641_a_(new StringTextComponent("scallop"), ChatType.SYSTEM, Util.DUMMY_UUID);
 			}
-		} else if (((sel >= 24) && (sel <= 30))) {
+		} else if (((sel >= (scallop + 1)) && (sel <= seaurchin))) {
 			{
 				Entity _ent = entity;
 				if (!_ent.world.isRemote && _ent.world.getServer() != null) {
@@ -195,7 +221,7 @@ public class ColdOceanFishProcedure {
 				if (mcserv != null)
 					mcserv.getPlayerList().func_232641_a_(new StringTextComponent("sea urchin"), ChatType.SYSTEM, Util.DUMMY_UUID);
 			}
-		} else if (((sel >= 31) && (sel <= 37))) {
+		} else if (((sel >= (seaurchin + 1)) && (sel <= crab))) {
 			{
 				Entity _ent = entity;
 				if (!_ent.world.isRemote && _ent.world.getServer() != null) {
@@ -211,7 +237,7 @@ public class ColdOceanFishProcedure {
 				if (mcserv != null)
 					mcserv.getPlayerList().func_232641_a_(new StringTextComponent("crab"), ChatType.SYSTEM, Util.DUMMY_UUID);
 			}
-		} else if (((sel >= 38) && (sel <= 45))) {
+		} else if (((sel >= (crab + 1)) && (sel <= jellyfish))) {
 			{
 				Entity _ent = entity;
 				if (!_ent.world.isRemote && _ent.world.getServer() != null) {
@@ -231,14 +257,113 @@ public class ColdOceanFishProcedure {
 		if (!world.isRemote()) {
 			MinecraftServer mcserv = ServerLifecycleHooks.getCurrentServer();
 			if (mcserv != null)
+				mcserv.getPlayerList().func_232641_a_(new StringTextComponent("selection"), ChatType.SYSTEM, Util.DUMMY_UUID);
+		}
+		if (!world.isRemote()) {
+			MinecraftServer mcserv = ServerLifecycleHooks.getCurrentServer();
+			if (mcserv != null)
 				mcserv.getPlayerList().func_232641_a_(new StringTextComponent((new java.text.DecimalFormat("##.##").format(sel))), ChatType.SYSTEM,
 						Util.DUMMY_UUID);
 		}
 		if (!world.isRemote()) {
 			MinecraftServer mcserv = ServerLifecycleHooks.getCurrentServer();
 			if (mcserv != null)
+				mcserv.getPlayerList().func_232641_a_(new StringTextComponent("total fish count"), ChatType.SYSTEM, Util.DUMMY_UUID);
+		}
+		if (!world.isRemote()) {
+			MinecraftServer mcserv = ServerLifecycleHooks.getCurrentServer();
+			if (mcserv != null)
 				mcserv.getPlayerList().func_232641_a_(new StringTextComponent((new java.text.DecimalFormat("##.##").format(fish))), ChatType.SYSTEM,
 						Util.DUMMY_UUID);
+		}
+		if (!world.isRemote()) {
+			MinecraftServer mcserv = ServerLifecycleHooks.getCurrentServer();
+			if (mcserv != null)
+				mcserv.getPlayerList().func_232641_a_(new StringTextComponent("luck"), ChatType.SYSTEM, Util.DUMMY_UUID);
+		}
+		if (!world.isRemote()) {
+			MinecraftServer mcserv = ServerLifecycleHooks.getCurrentServer();
+			if (mcserv != null)
+				mcserv.getPlayerList().func_232641_a_(new StringTextComponent((new java.text.DecimalFormat("##.##").format(luck))), ChatType.SYSTEM,
+						Util.DUMMY_UUID);
+		}
+		if (!world.isRemote()) {
+			MinecraftServer mcserv = ServerLifecycleHooks.getCurrentServer();
+			if (mcserv != null)
+				mcserv.getPlayerList().func_232641_a_(new StringTextComponent((new java.text.DecimalFormat("##.##").format(unluck))), ChatType.SYSTEM,
+						Util.DUMMY_UUID);
+		}
+		if (!world.isRemote()) {
+			MinecraftServer mcserv = ServerLifecycleHooks.getCurrentServer();
+			if (mcserv != null)
+				mcserv.getPlayerList().func_232641_a_(new StringTextComponent((new java.text.DecimalFormat("##.##").format(totalluck))),
+						ChatType.SYSTEM, Util.DUMMY_UUID);
+		}
+		if (!world.isRemote()) {
+			MinecraftServer mcserv = ServerLifecycleHooks.getCurrentServer();
+			if (mcserv != null)
+				mcserv.getPlayerList().func_232641_a_(new StringTextComponent("cod"), ChatType.SYSTEM, Util.DUMMY_UUID);
+		}
+		if (!world.isRemote()) {
+			MinecraftServer mcserv = ServerLifecycleHooks.getCurrentServer();
+			if (mcserv != null)
+				mcserv.getPlayerList().func_232641_a_(new StringTextComponent((new java.text.DecimalFormat("##.##").format(cod))), ChatType.SYSTEM,
+						Util.DUMMY_UUID);
+		}
+		if (!world.isRemote()) {
+			MinecraftServer mcserv = ServerLifecycleHooks.getCurrentServer();
+			if (mcserv != null)
+				mcserv.getPlayerList().func_232641_a_(new StringTextComponent("salmon"), ChatType.SYSTEM, Util.DUMMY_UUID);
+		}
+		if (!world.isRemote()) {
+			MinecraftServer mcserv = ServerLifecycleHooks.getCurrentServer();
+			if (mcserv != null)
+				mcserv.getPlayerList().func_232641_a_(new StringTextComponent((new java.text.DecimalFormat("##.##").format(salmon))), ChatType.SYSTEM,
+						Util.DUMMY_UUID);
+		}
+		if (!world.isRemote()) {
+			MinecraftServer mcserv = ServerLifecycleHooks.getCurrentServer();
+			if (mcserv != null)
+				mcserv.getPlayerList().func_232641_a_(new StringTextComponent("scallop"), ChatType.SYSTEM, Util.DUMMY_UUID);
+		}
+		if (!world.isRemote()) {
+			MinecraftServer mcserv = ServerLifecycleHooks.getCurrentServer();
+			if (mcserv != null)
+				mcserv.getPlayerList().func_232641_a_(new StringTextComponent((new java.text.DecimalFormat("##.##").format(scallop))),
+						ChatType.SYSTEM, Util.DUMMY_UUID);
+		}
+		if (!world.isRemote()) {
+			MinecraftServer mcserv = ServerLifecycleHooks.getCurrentServer();
+			if (mcserv != null)
+				mcserv.getPlayerList().func_232641_a_(new StringTextComponent("urchin"), ChatType.SYSTEM, Util.DUMMY_UUID);
+		}
+		if (!world.isRemote()) {
+			MinecraftServer mcserv = ServerLifecycleHooks.getCurrentServer();
+			if (mcserv != null)
+				mcserv.getPlayerList().func_232641_a_(new StringTextComponent((new java.text.DecimalFormat("##.##").format(seaurchin))),
+						ChatType.SYSTEM, Util.DUMMY_UUID);
+		}
+		if (!world.isRemote()) {
+			MinecraftServer mcserv = ServerLifecycleHooks.getCurrentServer();
+			if (mcserv != null)
+				mcserv.getPlayerList().func_232641_a_(new StringTextComponent("crab"), ChatType.SYSTEM, Util.DUMMY_UUID);
+		}
+		if (!world.isRemote()) {
+			MinecraftServer mcserv = ServerLifecycleHooks.getCurrentServer();
+			if (mcserv != null)
+				mcserv.getPlayerList().func_232641_a_(new StringTextComponent((new java.text.DecimalFormat("##.##").format(crab))), ChatType.SYSTEM,
+						Util.DUMMY_UUID);
+		}
+		if (!world.isRemote()) {
+			MinecraftServer mcserv = ServerLifecycleHooks.getCurrentServer();
+			if (mcserv != null)
+				mcserv.getPlayerList().func_232641_a_(new StringTextComponent("jelly"), ChatType.SYSTEM, Util.DUMMY_UUID);
+		}
+		if (!world.isRemote()) {
+			MinecraftServer mcserv = ServerLifecycleHooks.getCurrentServer();
+			if (mcserv != null)
+				mcserv.getPlayerList().func_232641_a_(new StringTextComponent((new java.text.DecimalFormat("##.##").format(jellyfish))),
+						ChatType.SYSTEM, Util.DUMMY_UUID);
 		}
 	}
 }
