758915aacbfd0d21c73d95f8f5444ec2fb08fe2e
eggggegggge <4601242@CTC3220.spsd.sk.ca>
2021-10-14 09:50:24 -0600

    ocean stuff idk

---
+++ src/main/java/net/mcreator/thewetsmp/procedures/RiverTreasureProcedure.java
@@ -0,0 +1,77 @@
+package net.mcreator.thewetsmp.procedures;
+
+import net.minecraft.world.World;
+import net.minecraft.world.IWorld;
+import net.minecraft.entity.item.ExperienceOrbEntity;
+import net.minecraft.entity.Entity;
+
+import net.mcreator.thewetsmp.TheWetSmpRehydratedMod;
+
+import java.util.Random;
+import java.util.Map;
+import java.util.HashMap;
+
+public class RiverTreasureProcedure {
+	public static void executeProcedure(Map<String, Object> dependencies) {
+		if (dependencies.get("entity") == null) {
+			if (!dependencies.containsKey("entity"))
+				TheWetSmpRehydratedMod.LOGGER.warn("Failed to load dependency entity for procedure RiverTreasure!");
+			return;
+		}
+		if (dependencies.get("y") == null) {
+			if (!dependencies.containsKey("y"))
+				TheWetSmpRehydratedMod.LOGGER.warn("Failed to load dependency y for procedure RiverTreasure!");
+			return;
+		}
+		if (dependencies.get("x") == null) {
+			if (!dependencies.containsKey("x"))
+				TheWetSmpRehydratedMod.LOGGER.warn("Failed to load dependency x for procedure RiverTreasure!");
+			return;
+		}
+		if (dependencies.get("z") == null) {
+			if (!dependencies.containsKey("z"))
+				TheWetSmpRehydratedMod.LOGGER.warn("Failed to load dependency z for procedure RiverTreasure!");
+			return;
+		}
+		if (dependencies.get("world") == null) {
+			if (!dependencies.containsKey("world"))
+				TheWetSmpRehydratedMod.LOGGER.warn("Failed to load dependency world for procedure RiverTreasure!");
+			return;
+		}
+		Entity entity = (Entity) dependencies.get("entity");
+		double y = dependencies.get("y") instanceof Integer ? (int) dependencies.get("y") : (double) dependencies.get("y");
+		double x = dependencies.get("x") instanceof Integer ? (int) dependencies.get("x") : (double) dependencies.get("x");
+		double z = dependencies.get("z") instanceof Integer ? (int) dependencies.get("z") : (double) dependencies.get("z");
+		IWorld world = (IWorld) dependencies.get("world");
+		double sel = 0;
+		double time = 0;
+		double altitude = 0;
+		double luck = 0;
+		double crate = 0;
+		double treasure = 0;
+		/* loot factors */
+		time = (double) (world.getWorldInfo().getDayTime());
+		altitude = (double) (Math.floor(y));/* define base values */
+		crate = (double) 4;/* treasure */
+		treasure = (double) crate;
+		sel = (double) ((new Random()).nextInt((int) treasure + 1));
+		if (((sel >= 0) && (sel <= crate))) {
+			{
+				Map<String, Object> $_dependencies = new HashMap<>();
+				$_dependencies.put("entity", entity);
+				$_dependencies.put("world", world);
+				RemoveBaitProcedure.executeProcedure($_dependencies);
+			}
+			{
+				Entity _ent = entity;
+				if (!_ent.world.isRemote && _ent.world.getServer() != null) {
+					_ent.world.getServer().getCommandManager().handleCommand(_ent.getCommandSource().withFeedbackDisabled().withPermissionLevel(4),
+							"give @s the_wet_smp_rehydrated:river_crate{new:1} 1");
+				}
+			}
+			if (world instanceof World && !world.isRemote()) {
+				((World) world).addEntity(new ExperienceOrbEntity(((World) world), x, (y - 0.5), z, (int) 8));
+			}
+		}
+	}
+}
